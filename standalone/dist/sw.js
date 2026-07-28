dfda288c6f7cb87f16d30eb1d7b9d42fd2441a23
kniessner <sascha-darius@kniessner.com>
2026-07-28 16:20:53 +0200

    go live

--- standalone/dist/sw.js
+++ standalone/dist/sw.js
@@ -7,7 +7,7 @@
  *   - Cross-origin assets        → Cache-First only for CORS/basic responses
  */
 
-const CACHE_VERSION = '1.5.0';
+const CACHE_VERSION = '1.5.1';
 const APP_NAME = 'bucht-standalone';
 const CACHE_NAME = `${APP_NAME}-v${CACHE_VERSION}`;
 
@@ -36,7 +36,7 @@ const SHELL_ASSETS = [
     './manifest.json',
     './icons/icon-192.png',
     './icons/icon-512.png',
-    './images/bg.jpg',
+    './images/frame.png',
     './images/datum.png',
     './data/info.json',
     './data/timetable.json',
