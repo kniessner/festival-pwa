5691b32dc012dc1d850ee220a428713a8a069886
kniessner <sascha-darius@kniessner.com>
2026-07-28 17:45:57 +0200

    go live

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  *   - Cross-origin assets        → Cache-First only for CORS/basic responses
  */
 
-const CACHE_VERSION = '1.5.11';
+const CACHE_VERSION = '1.5.12';
 const APP_NAME = 'bucht-standalone';
 const CACHE_NAME = `${APP_NAME}-v${CACHE_VERSION}`;
 
