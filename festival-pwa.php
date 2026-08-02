c5836d4e1d6edc0c7b6e17ad63c4a55664f7a103
kniessner <sascha-darius@kniessner.com>
2026-08-02 14:25:59 +0200

    feat(music-import): load class-music-import.php from the plugin bootstrap

--- festival-pwa.php
+++ festival-pwa.php
@@ -18,6 +18,7 @@ require_once FESTIVAL_PWA_DIR . 'includes/class-rest-api.php';
 require_once FESTIVAL_PWA_DIR . 'includes/class-pwa-frontend.php';
 require_once FESTIVAL_PWA_DIR . 'includes/class-notifications.php';
 require_once FESTIVAL_PWA_DIR . 'includes/class-music.php';
+require_once FESTIVAL_PWA_DIR . 'includes/class-music-import.php';
 
 class Festival_PWA {
     /**
