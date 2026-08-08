c41c79dd33d265916ea50de5a091b6238c8ba360
kubante <kubante@gmail.com>
2026-08-08 14:46:56 +0200

    map(camera): default pitch 20\u00b0 \u2192 25\u00b0

--- standalone/js/views/map-interactive.js
+++ standalone/js/views/map-interactive.js
@@ -66,7 +66,7 @@ const DEFAULT_CAMERA = {
     center: [14.496068, 52.274408],
     zoom: 14.33,
     bearing: -78.1,
-    pitch: 20,
+    pitch: 25,
 };
 
 // Pan-limit bounds — hand-picked by Jacob (2026-08-08) from a felt
