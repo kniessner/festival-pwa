0283e052c8e164ba8027ec62584153f3bd6e19fe
kubante <kubante@gmail.com>
2026-08-10 10:11:41 +0200

    data(sanitize): rename Re:set Raversnacks -> Raversnacks

--- standalone/sw.js
+++ standalone/sw.js
@@ -7,7 +7,7 @@
  *   - Cross-origin assets        → Cache-First only for CORS/basic responses
  */
 
-const CACHE_VERSION = '1786349390';
+const CACHE_VERSION = '1786349495';
 const APP_NAME = 'bucht-standalone';
 const CACHE_NAME = `${APP_NAME}-v${CACHE_VERSION}`;
 
