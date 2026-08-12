00d638d97d2254f07fee15d5ef5cacc79be65d41
kniessner <sascha-darius@kniessner.com>
2026-08-12 10:20:17 +0200

    Update asset versioning and cache version for deployment

--- standalone/sw.js
+++ standalone/sw.js
@@ -7,7 +7,7 @@
  *   - Cross-origin assets        → Cache-First only for CORS/basic responses
  */
 
-const CACHE_VERSION = '1786438775';
+const CACHE_VERSION = '1786522793';
 const APP_NAME = 'bucht-standalone';
 const CACHE_NAME = `${APP_NAME}-v${CACHE_VERSION}`;
 
